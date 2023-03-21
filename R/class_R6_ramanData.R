7a8c66b4be6b2969d0da0fb17d8e322d96ed63a5
JanaThissen <thissen@iuta.de>
2023-03-21 13:52:35 +0100

    Create class_R6_ramanData.R

---
+++ R/class_R6_ramanData.R
@@ -0,0 +1,12 @@
+#' **ramanData** R6 class and methods
+#'
+#' @description
+#' The msData R6 class is a framework with methods for parsing, processing,
+#' visualizing and storing RAMAN data.
+#'
+#' @export
+#'
+msData <- R6::R6Class("ramanData",
+  private = list(),
+  public = list()
+)
