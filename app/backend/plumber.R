11e27f0fe891984bb50a4426e083f7e5f9664716
walt <Lauritowal@yahoo.com>
2023-04-20 20:12:11 +0100

    add example

--- app/backend/plumber.R
+++ app/backend/plumber.R
@@ -1,4 +1,4 @@
-# plumber.R
+source("../../R/class_R6_msData.R")
 
 #* Echo back the input
 #* @param msg The message to echo
@@ -7,18 +7,10 @@ function(msg="") {
   list(msg = paste0("The message is: '", msg, "'"))
 }
 
-#* Plot a histogram
-#* @serializer png
-#* @get /plot
+#* Echo back the input
+#* @get /test
 function() {
-  rand <- rnorm(100)
-  hist(rand)
-}
-
-#* Return the sum of two numbers
-#* @param a The first number to add
-#* @param b The second number to add
-#* @post /sum
-function(a, b) {
-  as.numeric(a) + as.numeric(b)
+  ms <- msData$new()
+  overview = ms$get_blank_names()
+  return(overview)
 }
