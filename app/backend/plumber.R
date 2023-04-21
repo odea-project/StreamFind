b962893617c4b7b1e75bf60bcb1c987582404c3b
walt <Lauritowal@yahoo.com>
2023-04-21 17:27:04 +0100

    add plumber example

--- app/backend/plumber.R
+++ app/backend/plumber.R
@@ -1,16 +1,11 @@
-source("../../R/class_R6_msData.R")
+library(streamFind)
+# source("../../R/class_R6_msData.R")
 
 #* Echo back the input
-#* @param msg The message to echo
-#* @get /echo
-function(msg="") {
-  list(msg = paste0("The message is: '", msg, "'"))
-}
-
-#* Echo back the input
-#* @get /test
+#* @get /connect
 function() {
-  ms <- msData$new()
-  overview = ms$get_blank_names()
-  return(overview)
-}
+  files <- streamFindData::msFilePaths()[1:3]
+  ms <- msData$new(files)
+  names = ms$get_analysis_names()
+  return(unclass(names))
+} # TODO: look into install patRoon from fork
