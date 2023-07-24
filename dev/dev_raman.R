de3be41a00935bccf47c85d1e3a5c8e212784e58
Ricardo Cunha <cunha@iuta.de>
2023-07-24 12:59:19 +0200

    Added extra functions, updated new method and some documentation

--- dev/dev_raman.R
+++ dev/dev_raman.R
@@ -1,9 +1,28 @@
 
-files <- list.files("F:/", full.names = TRUE)
+files <- list.files("D:/Jana/", full.names = TRUE)
 files <- files[1:2]
-r1 <- ramanData$new(files)
+
+
+r1 <- RamanData$new(files, headers = list(
+  "name" = "Example files",
+  "description" = "Test Raman class."
+))
+
+r1
+
+
+r1$get_spectra(shift = c(300, 100))
+
+plot(r1$get_spectra(shift = c(300, 100))[, 2:3])
+
+
+
+plot(r1$get_spectra(2)[1:200, 2:3])
 
 View(r1$get_analyses())
+
+
+
 
 
 
