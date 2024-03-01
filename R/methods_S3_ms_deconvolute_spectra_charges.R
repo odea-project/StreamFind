6b24f6fbe2a4a7070a3097ccd05b7c1aedab5da9
Ricardo Cunha <64540212+ricardo-cunha@users.noreply.github.com>
2024-03-01 16:26:48 +0100

    Added deconvolution of spectra charges

--- R/methods_S3_ms_deconvolute_spectra_charges.R
+++ R/methods_S3_ms_deconvolute_spectra_charges.R
@@ -7,17 +7,33 @@
 #'
 .s3_deconvolute_spectra_charges.Settings_deconvolute_spectra_charges_StreamFind <- function(settings, self) {
   
+  intensity <- NULL
   
-  rtmin = 314 - 5
-  rtmax = 314 + 5
-  mzmin = 2500
-  mzmax = 3600
+  rtmin = 315 - 2.5
+  rtmax = 315 + 2.5
+  mzmin = 2400
+  mzmax = 3800
   presence = 0.1 # presence can be adjusted to remove noise
   mzClust = 0.001 # critical for resolution, in Da
   minIntensity = 50
-  roundVal = 30
+  roundVal = 35
   lowCut = 0.2
-  plotLevel = 2
+  mzClustAverage = 0.1
+  smoothing = TRUE
+  windowSize = 25
+  baseline = TRUE
+  baseline_method = "als"
+  baseline_args = list(lambda = 9, p = 0.02, maxit = 10)
+  merge = TRUE
+  closeByThreshold = 45
+  valeyThreshold = 0.5
+  minPeakHeight = 100
+  minPeakDistance = 50
+  maxPeakWidth = 250
+  minPeakWidth = 50
+  
+  plotLevel = 4
+  
   xlab = expression(italic("m/z"))
   ylab = "Intensity / counts"
   title = "Charges and mass annotation"
@@ -31,14 +47,302 @@
   
   # .plot_spectra_interactive(ms1)
   
-  browser()
+  ms1_list <- split(ms1, ms1$analysis)
+  
+  ms1_list <- lapply(ms1_list, function(x) {
+    
+    if (nrow(x) == 0) return(data.table())
+    
+    if ("BVCZ_Blank" %in% unique(x$analysis)) return(list())
+    
+    x$cluster <- round(x$mz / roundVal) * roundVal
+    
+    sp <- copy(x)
+    setorder(sp, -intensity)
+    sp$mzLow <- NA_real_
+    sp$mzHigh <- NA_real_
+    sp$intLow <- NA_real_
+    sp$intHigh <- NA_real_
+    
+    clusters <- unique(sp$cluster)
+    
+    for (i in seq_len(length(clusters))) {
+      temp <- sp[sp$cluster == clusters[i], ]
+      sp$mzLow[sp$cluster == clusters[i]] <- min(temp$mz)
+      sp$mzHigh[sp$cluster == clusters[i]] <- max(temp$mz)
+      sp$intLow[sp$cluster == clusters[i]] <- min(temp$intensity)
+      sp$intHigh[sp$cluster == clusters[i]] <- max(temp$intensity)
+      mzMax <- temp$mz[temp$intensity == max(temp$intensity)]
+      sp$mz[sp$cluster == clusters[i]] <- mzMax[1]
+    }
+    
+    sp2 <- sp[, .(intensity = max(intensity)), by = c("mz", "cluster")]
+    
+    setorder(sp2, -mz)
+    
+    for (i in seq_len(nrow(sp2))) {
+      
+      if (is.na(sp2$mz[i + 1])) next
+      
+      if (sp2$mz[i] - sp2$mz[i + 1] < roundVal) {
+        
+        if (sp2$intensity[i] >= sp2$intensity[i + 1]) {
+          sel <- sp2$mz %in% sp2$mz[i + 1]
+          sp2$mz[sel] <- sp2$mz[i]
+          sp2$cluster[sel] <- sp2$cluster[i]
+          sp2$intensity[sel] <- sp2$intensity[i]
+          
+        } else {
+          sel <- sp2$mz %in% sp2$mz[i]
+          sp2$mz[sel] <- sp2$mz[i + 1]
+          sp2$cluster[sel] <- sp2$cluster[i + 1]
+          sp2$intensity[sel] <- sp2$intensity[i + 1]
+        }
+      }
+    }
+    
+    sp2 <- unique(sp2)
+    
+    setorder(sp2, -intensity)
+    
+    sp2 <- sp2[sp2$intensity / sp2$intensity[1] > lowCut, ]
+    
+    if (plotLevel > 1) {
+      
+      plot(x$mz, x$intensity, type = 'l', ylab = ylab, xlab = xlab,
+        main = "Clusters, overlap window and low intensity threshold (lowCut)"
+      )
+      
+      aboveLowCut <- sp$cluster %in% sp2$cluster
+      aboveLowCut <- sp[aboveLowCut, ]
+      
+      rect(
+        aboveLowCut$mzLow,
+        aboveLowCut$intLow,
+        aboveLowCut$mzHigh,
+        aboveLowCut$intHigh,
+        border = "lightgreen",
+        lty = 2,
+        lwd = 0.8,
+        angle = 30
+      )
+      
+      belowLowCut <- !sp$cluster %in% sp2$cluster
+      belowLowCut <- sp[belowLowCut, ]
+      
+      rect(
+        belowLowCut$mzLow,
+        belowLowCut$intLow,
+        belowLowCut$mzHigh,
+        belowLowCut$intHigh,
+        border = "darkred",
+        lty = 2,
+        lwd = 0.8,
+        angle = 30
+      )
+      
+      abline(h = sp2$intensity[1] * lowCut, col = "darkred")
+      
+      text(
+        sp2$mz,
+        sp2$intensity,
+        labels = seq_len(nrow(sp2)),
+        adj = c(0.2, 0),
+        col = "darkgreen"
+      )
+      
+      rect(
+        sp2$mz - roundVal,
+        0,
+        sp2$mz + roundVal,
+        sp2$intensity,
+        border = "darkgreen",
+        lty = 1,
+        lwd = 1,
+        angle = 30
+      )
+      
+      legend(
+        x = "topright",
+        legend = c("Clusters", "lowCut", "Excluded clusters", "Final clusters & window"),
+        col = c("lightgreen", "darkred", "darkred", "darkgreen"),
+        lwd = 2,
+        lty = c(2, 1, 2, 1),
+        cex = 0.7,
+        bty = "n"
+      )
+    }
+    
+    res <- x[x$mz == 0, ]
+    
+    for (i in seq_len(nrow(sp2))) {
+      temp <- x[x$cluster == sp2$cluster[i] & x$mz == sp2$mz[i], ]
+      res <- rbind(res, temp)
+    }
+    
+    res <- unique(res)
+    
+    setorder(res, mz)
+    
+    res$z <- NA_integer_
+    
+    # Based on the relationship
+    #
+    # m/z_iZ = m/z_{i+1}(Z-1)
+    #
+    # giving
+    #
+    # Z = \frac{-m/z_{i+1}}{m/z_i-m/z_{i+1}}
+    
+    for (i in seq_len(nrow(res))) {
+      res$z[i] <- round((-res$mz[i + 1]) / (res$mz[i] - res$mz[i + 1]), digits = 0)
+    }
+    
+    res <- res[!is.na(res$z), ]
+    
+    res <- res[-1, ] # removes the first as mass estimation might be affected by not complete cluster
+    
+    res$mass <- NA_real_
+    
+    for (i in seq_len(nrow(res))) res$mass[i] <- res$mz[i] * res$z[i]
+    
+    res_outliers <- res[res$mass < (mean(res$mass) - sd(res$mass)) | res$mass > (mean(res$mass) + sd(res$mass))]
+    
+    if (nrow(res_outliers) > 0) {
+      res <- res[res$cluster != res_outliers$cluster, ]
+    }
+    
+    if (plotLevel > 0) {
+      plot(
+        x$mz, x$intensity, type = 'l',
+        ylab = ylab,
+        xlab = xlab,
+        main = title,
+        ylim = c(0, max(x$intensity) * 1.4)
+      )
+      
+      text(
+        x = res$mz, y = res$intensity, adj = c(-0.1, 0.25),
+        labels = paste0(round(res$mz, digits = 3), " (+", res$z, ")"),
+        vfont = NULL, cex = 0.6, col = "darkgreen", font = NULL, srt = 90
+      )
+      
+      if (nrow(res_outliers) > 0) {
+        text(
+          x = res_outliers$mz, y = res_outliers$intensity, adj = c(-0.1, 0.25),
+          labels = paste0(round(res_outliers$mz, digits = 3), " (+", res_outliers$z, ")"),
+          vfont = NULL, cex = 0.6, col = "darkred", font = NULL, srt = 90
+        )
+      }
+    }
+    
+    profiles <- lapply(seq_len(nrow(res)), function(j) {
+      
+      window <- (res$mz[j + 1] - res$mz[j]) / 2
+      
+      sel <- x$mz >= (res$mz[j] - window) & x$mz <= (res$mz[j] + window)
+      
+      prfl <- x[sel, ]
+      
+      if (max(res$intensity) == res$intensity[j] & plotLevel > 1) {
+        plot(prfl$mz, prfl$intensity, type = 'l',
+             xlab = expression(italic("m/z")),
+             ylab = ylab,
+             main = paste0("Raw profile for charge +", res$z[j])
+        )
+      }
+      
+      decon_prfl <- prfl
+      
+      decon_prfl$mz <- prfl$mz * res$z[j]
+      
+      if (max(res$intensity) == res$intensity[j] & plotLevel > 1) {
+        plot(
+          decon_prfl$mz, decon_prfl$intensity, type = 'l',
+          xlab = xlab,
+          ylab = ylab,
+          main = paste0("Deconvoluted bvz spectra for charge +", res$z[j]),
+          ylim = c(0, max(decon_prfl$intensity) * 1.4)
+        )
+      }
+      
+      decon_prfl
+    })
+    
+    profiles <- profiles[vapply(profiles, function(x) nrow(x) > 0, FALSE)]
+    
+    profiles_dt <- rbindlist(profiles)
+    
+    profiles_dt$unique_id <- "var"
+    
+    av_profile <- rcpp_ms_cluster_spectra(profiles_dt, mzClust = mzClustAverage, presence = 0)[[1]]
+    
+    av_profile <- as.data.table(av_profile)
+    
+    setnames(av_profile, "mz", "mass")
+    
+    if (plotLevel > 1) {
+      plot(av_profile$mass, av_profile$intensity, type = 'l', xlab = xlab, ylab = ylab,
+        main = paste0("Merged spectra by weighted average with mzClust ", mzClustAverage)
+      )
+    }
+    
+    if (smoothing || baseline) av_profile$raw <- av_profile$intensity
+    
+    if (smoothing) {
+      av_profile$smoothed <- .moving_average(av_profile$intensity, windowSize)
+      av_profile$intensity <- av_profile$smoothed
+    }
+    
+    
+    
+    if (plotLevel > 1) {
+      plot(av_profile$mass, av_profile$intensity, type = 'l', xlab = xlab, ylab = ylab,
+        main = paste0("Smoothed profile")
+      )
+    }
+    
+    if (baseline) {
+      baseline_out <- .baseline_correction(av_profile$intensity, baseline_method, baseline_args)
+      av_profile$baseline <- baseline_out$baseline
+      av_profile$corrected <- baseline_out$corrected
+      av_profile$intensity <- av_profile$corrected
+    }
+
+    setorder(av_profile, "mass")
+    
+    if (plotLevel > 1) {
+      plot(av_profile$mass, av_profile$intensity, type = "l", xlab = xlab, ylab = ylab, main = "Baseline correction")
+      if (smoothing) lines(av_profile$mass, av_profile$smoothed, col = "darkgreen")
+      if (baseline) lines(av_profile$mass, baseline_out$baseline, col = "darkred")
+    }
+
+    pks <- .find_peaks(av_profile, "mass", merge, closeByThreshold, valeyThreshold, minPeakHeight, minPeakDistance, maxPeakWidth, minPeakWidth)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    list(
+      "raw" = x,
+      "charges" = res,
+      "average" = list()
+    )
+  })
   
   
   
   
   
-  
-  
+  ms1_list
   
   
   
---
+++ R/utils_general.R
@@ -0,0 +1,357 @@
+
+#' @title .moving_average
+#' 
+#' @description Smooths a vector using a moving average window. Optionally, a time window can be given to also limit the
+#' window time.
+#' 
+#' @noRd
+#' 
+.moving_average <- function(vec, windowSize,  timeVec = NULL, timeWindow = NULL) {
+  
+  output <- numeric(length(vec))
+  
+  use_time <- FALSE
+  
+  if (is.numeric(timeVec) && length(timeVec) == length(vec) && is.numeric(timeWindow)) {
+    use_time <- TRUE
+  }
+  
+  all_idx <- seq_len(length(vec))
+  
+  for (z in seq_len(length(vec))) {
+    
+    if (use_time) {
+      idx <- which(abs(timeVec - timeVec[z]) <= timeWindow)
+      
+    } else {
+      idx <- all_idx
+    }
+    
+    left_window <- idx >= max(min(idx), z - windowSize) & idx < z
+    
+    left_window <- idx[left_window]
+    
+    left_size <- length(left_window)
+    
+    if (left_size > 0) left_window = vec[left_window]
+    
+    right_window <- idx <= min(max(idx), z + windowSize) & idx > z
+    
+    right_window <- idx[right_window]
+    
+    right_size <- length(right_window)
+    
+    if (right_size > 0) right_window <- vec[right_window]
+    
+    if (left_size == 0) {
+      output[z] <- vec[z]
+      next
+    }
+    
+    if (right_size == 0) {
+      output[z] <- vec[z]
+      next
+    }
+    
+    if (left_size < right_size) left_window <- c(left_window, rep(0, right_size - left_size))
+    
+    if (right_size < left_size) right_window <- c(right_window, rep(0, left_size - right_size))
+    
+    output[z] <- mean(c(left_window, vec[z], right_window))
+  }
+  
+  output
+}
+
+
+
+#' @title .baseline_correction
+#' 
+#' @description Corrects the baseline from a vector using the baseline package.
+#' 
+#' @noRd
+#' 
+.baseline_correction <- function(vec, method, opts) {
+  
+  mat <- matrix(as.numeric(vec), nrow = 1)
+  
+  mat <- do.call("baseline", c(list(spectra = mat, method = method), opts))
+  
+  i_baseline <- as.numeric(mat@baseline)
+  
+  i_baseline[i_baseline > vec] <- vec[i_baseline > vec]
+  
+  i_baseline[i_baseline < 0] <- vec[i_baseline < 0]
+
+  i_corrected <- vec - i_baseline
+  
+  i_corrected[i_corrected < 0] <- 0
+  
+  list("mat" = mat, "baseline" = i_baseline, "corrected" = i_corrected)
+}
+
+
+
+#' @title .find_peaks
+#' 
+#' @description Finds peaks in a given vector using the findpeaks function from pracma and an additional attempt 
+#' to merge peaks.
+#' 
+#' @noRd
+#' 
+.find_peaks <- function(data,
+                        xVar,
+                        merge = TRUE,
+                        closeByThreshold = 45,
+                        valeyThreshold = 0.5,
+                        minPeakHeight,
+                        minPeakDistance,
+                        maxPeakWidth,
+                        minPeakWidth) {
+  
+  plotLevel <- 4
+  
+  vec <- data$intensity
+  
+  xVec <- data[[xVar]]
+  
+  prac_pks <- pracma::findpeaks(
+    x = vec,
+    nups = 1,
+    ndowns = 1,
+    zero = "0",
+    peakpat = NULL,
+    minpeakheight = minPeakHeight, # Minimum peak height
+    minpeakdistance = minPeakDistance, # Minimum peak separation
+    threshold = 0, # Minimum height difference
+    npeaks = 0, # Maximum number of peaks
+    sortstr = FALSE # Peak sorting
+  )
+  
+  if (is.null(prac_pks)) {
+    return(data.table())
+    
+  } else {
+    pks <- data.table(
+      "idx" = as.integer(prac_pks[, 2]),
+      "xVal" = xVec[prac_pks[, 2]],
+      "min" = xVec[as.integer(prac_pks[, 3]) - 1],
+      "max" = xVec[as.integer(prac_pks[, 4]) + 1],
+      "intensity" = vec[as.integer(prac_pks[, 2])]
+    )
+  }
+  
+  if (plotLevel > 3) {
+    plot(xVec, vec, type = "l",main = "Found peaks")
+    points(pks$xVal, pks$intensity, pch = 16, col = "red", cex = 1.5)
+    text(x = pks$xVal, y = pks$intensity, adj = c(-0.4, 0.25),
+      labels = pks$index, cex = 0.6, col = "darkred", font = NULL, srt = 90
+    )
+  }
+  
+  setorder(pks, "xVal")
+  
+  pks$index <- seq_len(nrow(pks))
+  
+  if (merge) {
+    
+    pks$merged <- FALSE
+    
+    next_pk <- FALSE
+    
+    pk_pos <- 1
+    
+    for (i in seq_len(nrow(pks) - 1)) {
+      
+      if (next_pk) pk_pos <- i
+      
+      # if (i > 7) browser()
+      
+      pk_diff <- abs(pks$min[i + 1] - pks$max[i])
+      
+      pk_closeby <- pk_diff <= closeByThreshold
+      
+      if (pk_closeby) {
+        
+        pk_i <- vec[xVec == pks$xVal[i]]
+        
+        pk_next <- vec[xVec == pks$xVal[i + 1]]
+        
+        if (pk_i < pk_next) {
+          
+          pk_i <- vec[xVec == pks$max[i]]
+          
+          ints_between <- vec[xVec > pks$max[i] & xVec < pks$xVal[i + 1]]
+          
+          if (all(ints_between < pk_next)) {
+            
+            if (!all(ints_between > pk_i)) {
+              
+              if (length(ints_between[ints_between < pk_i]) / length(ints_between) < 0.5) {
+                do_merge <- TRUE 
+                
+              } else {
+                do_merge <- FALSE
+              }
+              
+            } else {
+              do_merge <- TRUE
+            }
+            
+          } else {
+            do_merge <- FALSE
+          }
+          
+        } else if (pk_i > pk_next) {
+          
+          pk_next <- vec[xVec == pks$min[i + 1]]
+          
+          ints_between <- vec[xVec > pks$xVal[i] & xVec < pks$min[i + 1]]
+          
+          if (all(ints_between < pk_i)) {
+            
+            if (!all(ints_between > pk_next)) {
+              
+              if (length(ints_between[ints_between < pk_next]) / length(ints_between) < 0.5) {
+                do_merge <- TRUE
+                
+              } else {
+                do_merge <- FALSE
+              }
+              
+            } else {
+              do_merge <- TRUE
+            }
+            
+          } else {
+            do_merge <- FALSE
+          }
+          
+        } else if (pk_i == pk_next) {
+          do_merge <- TRUE
+          
+        } else {
+          do_merge <- FALSE
+        }
+        
+        if (do_merge) {
+          pks$max[pk_pos] <- pks$max[i + 1]
+          t_i <- pks$intensity[pk_pos]
+          pks$intensity[pk_pos] <- max(pks$intensity[pk_pos], pks$intensity[i + 1])
+          if (t_i != pks$intensity[pk_pos]) pks$xVal[pk_pos] <- pks$xVal[i + 1]
+          pks$merged[i + 1] <- TRUE
+          next_pk <- FALSE
+          
+        } else {
+          next_pk <- TRUE
+        }
+        
+      } else {
+        next_pk <- TRUE
+      }
+    }
+    
+    if (plotLevel > 3) {
+      plot(xVec, vec, type = "l",main = "Found peaks")
+      points(pks$xVal, pks$intensity, pch = 16, col = "red", cex = 1.5)
+      points(pks$xVal[pks$merged], pks$intensity[pks$merged], pch = 16, col = "darkgreen", cex = 1.5)
+      text(x = pks$xVal, y = pks$intensity, adj = c(-0.4, 0.25),
+        labels = pks$index, cex = 0.6, col = "darkred", font = NULL, srt = 90
+      )
+    }
+    
+    pks <- pks[!pks$merged]
+  }
+
+  pks$width <- pks$max - pks$min
+  
+  pks <- pks[pks$width >= minPeakWidth & pks$width <= maxPeakWidth, ]
+  
+  if (nrow(pks) == 0) return(data.table())
+  
+  pks$index <- seq_len(nrow(pks))
+  
+  if ("smoothed" %in% colnames(data)) {
+    i_vec <- data$smoothed
+    
+  } else if ("raw" %in% colnames(data)) {
+    i_vec <- data$raw
+    
+  } else {
+    i_vec <- data$intensity
+  }
+  
+  pks$intensity <- vapply(pks$index, function(i) {
+    quarter_pk <- (pks$max[i] - pks$min[i]) / 4
+    max(i_vec[xVec > (pks$xVal[i] - quarter_pk) & xVec < (pks$xVal[i] + quarter_pk)])
+  }, 0)
+  
+  pks$xVal <- vapply(pks$index, function(i) {
+    quarter_pk <- (pks$max[i] - pks$min[i]) / 4
+    xVec[xVec > (pks$xVal[i] - quarter_pk) & xVec < (pks$xVal[i] + quarter_pk) & i_vec == pks$intensity[i]]
+  }, 0)
+  
+  pks$idx <- vapply(pks$index, function(i) which(xVec %in% pks$xVal[i]), 0)
+  
+  .trapezoidal_integration <- function(x, y) sum(diff(x) * (head(y, -1) + tail(y, -1)) / 2)
+  
+  .integrate_peak_area <- function(xVec, i_vec, baseline, peak_start, peak_end) {
+    peak_mask <- (xVec >= peak_start) & (xVec <= peak_end)
+    peak_xVec <- xVec[peak_mask]
+    if (!is.null(baseline)) {
+      peak_intensity <- i_vec[peak_mask] - baseline[peak_mask]
+    }
+    peak_intensity[peak_intensity < 0] <- 0
+    return(.trapezoidal_integration(peak_xVec, peak_intensity))
+  }
+  
+  if ("baseline" %in% colnames(data)) {
+    i_baseline <- data$baseline
+    
+  } else {
+    i_baseline <- NULL
+  }
+  
+  integrated_areas <- vapply(pks$index, function(i) {
+    .integrate_peak_area(xVec, i_vec, i_baseline, pks$min[i], pks$max[i])
+  }, 0)
+  
+  pks$area <- integrated_areas
+  
+  if (plotLevel > 0) {
+    
+    if ("smoothed" %in% colnames(data)) i_vec <- data$smoothed
+    
+    plot(xVec, i_vec, type = "l", col = "black",ylim = c(0, max(i_vec) * 1.1) )
+    
+    if ("baseline" %in% colnames(data)) lines(xVec, as.numeric(i_baseline), col = "darkred", lwd = 1, lty = 2)
+    
+    if (!"baseline" %in% colnames(data)) i_baseline <- rep(0, length(i_vec))
+    
+    colors <- .get_colors(pks$index)
+    
+    for (i in pks$index) {
+      
+      sel <- xVec > pks$min[i] & xVec < pks$max[i]
+      
+      lines(xVec[sel], i_vec[sel], pch = 16, col = colors[i], cex = 1.5)
+      
+      lines(xVec[sel], i_baseline[sel], pch = 16, col = colors[i], cex = 1.5)
+      
+      polygon(
+        c(xVec[sel], rev(xVec[sel])),
+        c(i_vec[sel], rev(i_baseline[sel])),
+        col = colors[i],
+        border = NA
+      )
+      
+      text(
+        x = pks$xVal[i], y = i_vec[pks$idx[i]], adj = c(0.5, -1),
+        labels = pks$index[i],
+        vfont = NULL, cex = 0.8, col = "black", font = NULL, srt = 0
+      )
+    }
+  }
+  
+  pks
+}
