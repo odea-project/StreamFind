5bb58d8210c199b9254d42c9ba580768824905e8
Ricardo Cunha <64540212+ricardo-cunha@users.noreply.github.com>
2025-09-10 15:21:16 +0200

    first show case for peak finding

--- dev/dev_peak_finding/dev_peak_finding_concept.R
+++ dev/dev_peak_finding/dev_peak_finding_concept.R
@@ -8,50 +8,91 @@ source("dev/dev_peak_finding/dev_plots.R")
 devtools::load_all()
 ms <- MassSpecEngine$new(analyses = files)
 
-#plot_spectra_eic(ms$Analyses, mass = db, colorBy = "targets")
+# plot_spectra_eic(
+#   ms$Analyses,
+#   mass = db[c(3, 6, 7, 10, 11), ],
+#   ppm = 20,
+#   sec = 1000,
+#   colorBy = "targets+analyses",
+#   legendNames = TRUE
+# )
 
-# MARK: Get_Spectra
-# Get_Spectra ------------------------------------------------------------------
+# MARK: Get Spectra
+# Get Spectra ------------------------------------------------------------------
+
+# TOF targets
+tof_target <- data.frame(
+  mzmin = 200,
+  mzmax = 250,
+  rtmin = 1100,
+  rtmax = 1200
+)
+
+# ORBITRAP targets
+orb_target <- data.frame(
+  mzmin = 200,
+  mzmax = 250,
+  rtmin = 700,
+  rtmax = 900
+)
+
 spec <- get_raw_spectra(
   ms$Analyses,
-  analyses = 2,
-  mz = data.frame(
-    mzmin = 200,
-    mzmax = 220,
-    rtmin = 1100,
-    rtmax = 1200
-  ),
-  ppm = 100,
-  sec = 200,
+  analyses = 3,
+  mz = orb_target,
   levels = 1
 )
 
-plot_3D_by_rt(spec, c(90))
+#plot_3D_by_rt(spec)
 
-unique_rt <- unique(spec$rt)
-length(unique_rt)
+# MARK: Peak Finding Concept
+# Peak Finding Concept ---------------------------------------------------------
 
+# args for TOF data
+args <- list(
+  noise = 500,
+  mzThreshold = 0.005
+)
+
+# args for Orbitrap data
+args <- list(
+  noise = 100000,
+  mzThreshold = 0.002
+)
+
+
+# MARK: Ordering m/z
 spec_ordered <- spec[order(spec$mz), ]
-spec_ordered <- spec_ordered[spec_ordered$intensity > 500, ]
 #plot_mz_vector(spec_ordered, interactive = FALSE)
 
+
+
+
+# MARK: Removes noise
+spec_ordered <- spec_ordered[spec_ordered$intensity > args$noise, ]
+#plot_mz_vector(spec_ordered, interactive = FALSE)
+
+
+
+
+# MARK: First derivative of m/z
 diff_mz <- diff(spec_ordered$mz)
 #plot_vector_scatter(diff_mz)
 
 
+
+
+# MARK: Clustering by m/z
 all_clusters <- integer(length(diff_mz))
-itMzClust <- 0.005
 for (j in seq_along(diff_mz)) {
-  if (diff_mz[j] > itMzClust) all_clusters[j] <- 1
+  if (diff_mz[j] > args$mzThreshold) all_clusters[j] <- 1
 }
 all_clusters <- cumsum(all_clusters)
 all_clusters <- c(0, all_clusters)
 all_clusters <- all_clusters + 1
 spec_ordered$cluster <- all_clusters
-
 counter <- table(spec_ordered$cluster)
 counter <- counter[counter > 5]
-
 spec_ordered$cluster[!spec_ordered$cluster %in% names(counter)] <- NA_integer_
 
 spec_ordered$mzmin <- NA_real_
@@ -72,21 +113,21 @@ clusters_summary <- spec_ordered[, .(
   rtmax = max(rt),
   intensity = sum(intensity),
   points = .N
-), by = .(cluster)]
+), by = .(cluster)][!is.na(cluster)]
 
-plot_mz_vector(spec_ordered, interactive = FALSE)
+#plot_mz_vector(spec_ordered, interactive = FALSE)
+#plot_3D_by_rt(spec_ordered)
 
-plot_3D_by_rt(spec_ordered)
 
+
+
+# MARK: Merging by rt
 # merge mz in each cluster with the same rt
+# for profile data, it actually centroids the data
+# in principle no effect on centroid data
 spec_ordered_merged <- spec_ordered[, .(
   mz = mean(mz),
   intensity = sum(intensity)
 ), by = .(rt, cluster)]
 
-plot_3D_by_rt(spec_ordered_merged)
-
-
-
-
-plot
+#plot_3D_by_rt(spec_ordered_merged)
--- dev/dev_peak_finding/dev_resources.R
+++ dev/dev_peak_finding/dev_resources.R
@@ -1,6 +1,21 @@
-all_files <- StreamFindData::get_ms_file_paths()
-files <- all_files[c(1, 7)]
-#files <- all_files[grepl("blank|influent|o3sw", all_files)]
+all_files_dir <- "C:\\Users\\apoli\\Documents\\example_files\\peak_finding_files_ex"
+all_files <- list.files(all_files_dir, full.names = TRUE, recursive = TRUE, pattern = "\\.mzML$")
+
+files_tof_cent <- all_files[grepl("tof_centroid", all_files)]
+files_tof_cent <- files_tof_cent[!grepl("wastewater", files_tof_cent)]
+files_tof_prof <- all_files[grepl("tof_profile", all_files)]
+files_orb_cent <- all_files[grepl("orbitrap_centroid", all_files)]
+files_orb_prof <- all_files[grepl("orbitrap_profile", all_files)]
+files_tof_ww <- all_files[grepl("wastewater", all_files)]
+
+files <- c(
+  files_tof_cent[1],
+  files_tof_prof[1],
+  files_orb_cent[7],
+  files_orb_prof[7],
+  files_tof_ww[11]
+)
+
 db_all <- StreamFindData::get_ms_tof_spiked_chemicals()
 db_all <- db_all[grepl("S", db_all$tag), ]
 cols <- c("name", "formula", "mass", "rt", "tag")
@@ -9,10 +24,12 @@ db_is <- db_is[, cols, with = FALSE]
 db_is <- db_is[!db_is$name %in% c("Ibuprofen-d3", "Naproxen-d3"), ]
 db_is_with_mz <- data.table::copy(db_is)
 db_is_with_mz$mz <- db_is_with_mz$mass + 1.007276
+db_is_with_mz$mass <- NULL
 db <- db_all[db_all$tag %in% "S", ]
 db <- db[, cols, with = FALSE]
 db_with_mz <- data.table::copy(db)
 db_with_mz$mz <- db_with_mz$mass + 1.007276
+db_with_mz$mass <- NULL
 db_with_ms2 <- StreamFindData::get_ms_tof_spiked_chemicals_with_ms2()
 db_with_ms2 <- db_with_ms2[db_with_ms2$tag %in% "S", ]
 db_with_ms2 <- db_with_ms2[, c("name", "formula", "mass", "SMILES", "rt", "polarity", "fragments"), with = FALSE]
